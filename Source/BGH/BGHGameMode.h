0511f095b0b2e91dca9369fb34f8a1e2ca47a128
jubalo <joaopaulonf@live.com>
2016-03-15 19:16:03 +0000

    Ficheiros de Início

---
+++ Source/BGH/BGHGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BGHGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BGH_API ABGHGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
